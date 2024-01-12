0729cbad0015a91cdc2f73a0f4eec6a730c5fc95
paul-petri <paul.petri@tss-yonder.com>
2024-01-12 17:32:05 +0200

    Added page view

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,9 @@
 import {Component, inject} from '@angular/core';
 import {AppStore} from './services/app.store';
+import { NavigationEnd, Router } from '@angular/router';
+import { filter } from 'rxjs';
+
+declare const gtag: Function;
 
 @Component({
   selector: 'app-root',
@@ -8,4 +12,16 @@ import {AppStore} from './services/app.store';
 })
 export class AppComponent {
   appStore = inject(AppStore);
+
+  constructor(private router: Router) {
+       /** START : Code to Track Page View using gtag.js */
+       this.router.events.pipe(
+        filter(event => event instanceof NavigationEnd)
+      ).subscribe((event: any) => {
+         gtag('event', 'page_view', {
+            page_path: event.urlAfterRedirects
+         })
+        })
+        /** END : Code to Track Page View  using gtag.js */
+  }
 }
